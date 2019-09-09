d7568b1cdccdce791b12b71fc53a8a18d3cf3c46
wzding <emma.ding@airbnb.com>
2019-09-08 17:59:04 -0700

    added file for another assignment

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -8,37 +8,21 @@ import { Student } from '../../shared/student.model';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent {
-    serverId = 10;
-    serverStatus = 'offLine';
-    counter = 0;
-    isLogin = false;
-    userName: string;
-    students: Student[] = [new Student('Bill Gates', 'Computer Science'),
-                           new Student('Steve Jobs', 'Computer Science'),
-                           new Student('Elon Musk', 'Computer Science')];
-
-    getServerStatus() {
-      return this.serverStatus;
+    isCheck = false;
+    dishName: string;
+//     students: Student[] = [new Student('Bill Gates', 'Computer Science'),
+//                            new Student('Steve Jobs', 'Computer Science'),
+//                            new Student('Elon Musk', 'Computer Science')]
+    check() {
+      this.isCheck = true;
     }
 
-    counterPlus() {
-     this.counter ++;
-    }
-
-    resetCounter() {
-      this.counter = 0;
-    }
-
-    login() {
-      this.isLogin = true;
-    }
-
-    signOut() {
-      this.isLogin = false;
-    }
+//     signOut() {
+//       this.isCheck = false;
+//     }
 
     // Event Binding
     onUpdateUserName(event: Event) {
-      this.userName = (<HTMLInputElement>event.target).value;
+      this.dishName = (<HTMLInputElement>event.target).value;
     }
 }
